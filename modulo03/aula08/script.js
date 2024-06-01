6a91c4af8fb62b900fc2cc089ef1fff27bf23c1a
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-06-01 12:49:22 -0300

    DOM, BOM, tratamento de erros, destructuring

---
+++ modulo03/aula08/script.js
@@ -0,0 +1,8 @@
+function trocarEstilizacao() {
+  document.getElementById('quadrado').className = 'estilo';
+}
+
+// function mudaTema() {
+//     const idBotao = document.body.style.backgroundColor = 'blue';
+//   if()
+// }
